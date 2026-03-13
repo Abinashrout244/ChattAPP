fa556bf0700875a0b85ee8e161518fde44675c24
Abinashrout244 <routabinash3775@gmail.com>
2026-03-14 00:23:24 +0530

    connect frontend to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,16 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const userRoute = require("./routes/auth.route");
 const messageRoute = require("./routes/message.route");
+const cors = require("cors");
 app.use(express.json()); //This is a built in middleware which is convert json -> js Object ,Provided by express
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/api/auth", userRoute);
 app.use("/api/msg", messageRoute);
